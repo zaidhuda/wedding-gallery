2fa906e7361be0b1ffbba448e0e6f312d2dfdd73
Zaid Huda <zaid.huda@surialabs.com>
2026-02-01 20:59:47 +0800

    feat: Introduce an admin panel for photo approval and deletion, and add a new guestbook page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,28 @@
 import { lazy, Suspense } from 'react';
-import { AppContextProvider } from './hooks/useContext';
-import HeroSection from './components/HeroSection';
-import FloatingNavigation from './components/FloatingNavigation';
 import { Route, Routes } from 'react-router';
-import useTheme from './hooks/useTheme';
-import './App.css';
-import GallerySection from './components/GallerySection';
 
-const MainContent = lazy(() => import('./components/MainContent'));
-
-function RenderApp() {
-  useTheme();
-
-  return (
-    <AppContextProvider>
-      <>
-        {/* <!-- Skip Navigation Link for Keyboard Users --> */}
-        <a href="#main-content" className="sr-only sr-only-focusable">
-          Skip to main content
-        </a>
-
-        <HeroSection />
-        <FloatingNavigation />
-
-        <Suspense fallback={<div>Loading...</div>}>
-          <MainContent />
-        </Suspense>
-      </>
-    </AppContextProvider>
-  );
-}
+const GallerySection = lazy(() => import('./components/GallerySection'));
+const Guestbook = lazy(() => import('./pages/Guestbook'));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RenderApp />}>
-        <Route path=":section" element={<GallerySection />} />
+      <Route
+        path="/"
+        element={
+          <Suspense>
+            <Guestbook />
+          </Suspense>
+        }
+      >
+        <Route
+          path=":section"
+          element={
+            <Suspense>
+              <GallerySection />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
